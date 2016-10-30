7650ca5dc04a1a9cac763617501a9e819993b572
Simon Wendel <mail@simonwendel.se>
2016-10-30 22:28:49 +0100

    feat: Add empty project and test project

---
+++ src/SimonWendel.ObjectExtensions.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ObjectExtensions.Tests")]
+[assembly: AssemblyDescription("Unit tests for ObjectExtensions")]
+[assembly: AssemblyProduct("ObjectExtensions.Tests")]
+[assembly: AssemblyCopyright("Copyright © Simon Wendel 2016")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e19a4f3c-99e0-422a-bcf2-a1df896f5464")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/SimonWendel.ObjectExtensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ObjectExtensions")]
+[assembly: AssemblyDescription("Extensions performing common operations on objects")]
+[assembly: AssemblyProduct("ObjectExtensions")]
+[assembly: AssemblyCopyright("Copyright © Simon Wendel 2016")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2400b18e-c787-4cd6-ace1-f2c9e53be404")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
